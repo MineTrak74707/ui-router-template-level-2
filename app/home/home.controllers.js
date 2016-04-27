4d2ecb82a8ea985d737eae0d1661dc50206a8a1b
Joshua Seah <joshuacseah@gmail.com>
2016-04-26 21:03:25 -0700

    first commit

--- app/home/home.controllers.js
+++ app/home/home.controllers.js
@@ -1,4 +1,42 @@
 angular.module('app')
-    .controller('ctrl',function($scope){
-        $scope.message='hello';
+    .controller('homeCtrl',function($scope, $window){
+        $scope.message = 'this is home';
+        $scope.inputModel = '';
+        $scope.things_todo = [];
+$scope.get_todo = function() {
+//Get todo items that are stored on the window.localStorage
+  var string = $window.localStorage.things_todo;
+  $scope.things_todo = string.split(',');
+};
+
+$scope.push_todo = function(item) {
+  $scope.things_todo.push(item);
+  $scope.inputModel = '';
+  $scope.keep_todos();
+};
+
+$scope.keep_todo = function() {
+  $window.localStorage.things_todo = $scope.things_todo;
+};
+
+$scope.delete = function() {
+//Delete a todo item on the list
+
+$scope.things_todo[index] = $scope.splice(index, 1);
+$scope.keep_todos();
+
+};
+
+$scope.edit = function() {
+$scope.things_todo[index] = $scope.inputModel;
+$scope.keep_todos();
+
+};
+
+$scope.read = function(input) {
+  $scope.inputModel = input;
+
+};
+
+
 });
